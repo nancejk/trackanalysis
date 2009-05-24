34b6412f5b32c1d81f8642d972bae33da3652e76
Jared Nance <nancejk@u.washington.edu>
2009-05-24 14:36:37 -0700

    Added usage information.

--- src/ta.cpp
+++ src/ta.cpp
@@ -4,8 +4,19 @@
 #include "TFile.h"
 #include <cstdlib>
 #include <string>
+
+//Print the correct usage syntax and exit() with return code 1.
+void UsageAndDie();
+
+//The main routine.  Loads the data file, creates the output file, and 
+//calls the function that will process the data.
 int main(int argc, const char* argv[])
 {
+	//If the incorrect number of arguments is passed, output usage information
+	//and die.
+	if ( argc != 3 ) UsageAndDie();
+	
+	//Otherwise...
 	std::string inFile(argv[1]);
 	std::string outFile(argv[2]);
 	TFile _file(outFile.c_str(),"RECREATE");
@@ -15,4 +26,16 @@ int main(int argc, const char* argv[])
 	std::cout << "Wrote " << _file.Sizeof() << 
 				" bytes on " << outFile << std::endl;
 	return EXIT_SUCCESS;
+}
+
+//Print appropriate usage information and die.
+void UsageAndDie()
+{
+	std::cout << "trackanalysis\n";
+	std::cout << "Photon process accounting post-processor for RAT data.\n";
+	std::cout << "Written by Jared Nance (University of Washington)\n";
+	std::cout << "\t nancejk@phys.washington.edu\n\n";
+	std::cout << "Usage:\n";
+	std::cout << "\t" << "trackanalysis <input file name> <output file name>";
+	exit(1);
 }
